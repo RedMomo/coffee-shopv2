ae32c29ec097de61b6cf57e0470d49e11c536d73
Maria Ameen <Ameen.maria.s@gmail.com>
2022-12-14 11:20:35 -0500

    updated routes to be used by shop page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+// import Home from './Home';
+import Shop from './Shop';
 
 export default function App() {
   return (
     <div>
-      {/* put the routes here */}
+      <Routes>
+        {/* <Route path="/" element={<Home />} /> */}
+        <Route path="/shop" element={<Shop products={products}/>} />
+      </Routes>
     </div>
   );
 }
